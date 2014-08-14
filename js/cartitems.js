d0fc59de9a3ff7bf58347fde1823380782c65f79
Matt Jones <mattpauljones@gmail.com>
2014-08-13 20:10:45 -0500

    updated products as well as added new files

---
+++ js/cartitems.js
@@ -0,0 +1,36 @@
+  
+  $(document).ready(function(){
+
+  $.getJSON("items.json", function(json) {
+
+    for(var i in json) {
+      $(".products").append("<div class=\"large-4 small-6 columns\"><img src=\"" + json[i].imgurl + "\"><div class=\"panel\"><h5>" + json[i].name + "</h5><h6 class= \"subheader item-price\"> $" + json[i].price +" </h6></div><div class=\"list-item-add\"><a href=\"#\" class=\"button\">Add to cart</a></div></div>"</div>");
+    }
+});
+
+  });
+
+  updateTotal = function() {
+    var total = 0;
+    $(".cart-item-price").each(function() {
+      total += parseInt($(this).html().substring(1));
+    });
+    $("#cart-total").html("$" + total);
+  };
+
+  $(document).on("click", ".list-item-add .button", function (e) {
+    e.preventDefault();
+    var $parent = $(this).parents(".list-item");
+    var name = $parent.find(".list-item-name").html();
+    var price = $parent.find(".list-item-price").html();
+    $("#cart tbody").append("<tr class=\"cart-item\"><td class=\"cart-item-name\">" + name + "</td><td class=\"cart-item-price\">" + price + "</td><td class=\"cart-item-remove\"><a href=\"#\" class=\"button\">Remove</a></td></tr>");
+    updateTotal();
+  });
+
+  $(document).on("click", ".cart-item-remove .button", function (e) {
+    e.preventDefault();
+    $(this).parents(".cart-item").remove();
+    updateTotal();
+  });
+
+});
